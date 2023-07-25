505e3d7749a8823510a6913e013aadd816f265ac
THEFIVEInc <126236955+THEFIVEInc@users.noreply.github.com>
2023-07-25 03:06:34 +0000

    main

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -114,8 +114,8 @@ const Home: NextPage = () => {
 </div>
 <div className="m-12 lg:m-32 px-12 py-6 lg:pt-8 lg:px-20 border-2 bg-black bg-opacity-30 text-center border-[#052ED9] bg-center bg-contain bg-no-repeat">
         <h1 className="text-2xl lg:text-4xl pt-2 lg:pt-4 lg:pb-2 text-white font-['Impact']">SEINA NFT</h1>
-        <h1 className="text-2xl lg:text-4xl pt-2 lg:pt-4 lg:pb-5 text-white font-['Impact']"> {mintNum} / 800</h1>
-        <a className="text-2xl lg:text-4xl pt-2 lg:pt-8 lg:pb-2 text-white font-['Impact']">4</a><a className="text-2xl lg:text-3xl pt-2 lg:pt-8 lg:pb-8 text-[#052ED9] font-['Impact'] ">MAX</a><br/>
+        <h1 className="text-2xl lg:text-4xl pt-2 lg:pt-4 lg:pb-5 text-white font-['Impact']"> {mintNum} / 333</h1>
+        <a className="text-2xl lg:text-4xl pt-2 lg:pt-8 lg:pb-2 text-white font-['Impact']">2</a><a className="text-2xl lg:text-3xl pt-2 lg:pt-8 lg:pb-8 text-[#052ED9] font-['Impact'] ">MAX</a><br/>
         <h1 className="text-2xl lg:text-4xl pt-2 lg:pt-4 lg:pb-2 text-white font-['Impact']">0.04 ETH</h1>
 
         <div className="pt-2 lg:pt-6 pb-11">
